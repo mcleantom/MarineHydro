6523ed3568d017f62e470a8669ba7ad3f7da336b
mcleantom <64887735+mcleantom@users.noreply.github.com>
2020-05-13 20:07:58 +0100

    plotting the wave

--- Thin Ship Theory/test.py
+++ Thin Ship Theory/test.py
@@ -14,12 +14,12 @@ import scipy.ndimage
 #from mpl_toolkits import mplot3d
 
 tank = tank.tank_properties()
-tank.M = 100  # Maximum number of wave harmonics to calculate
+tank.M = 150  # Maximum number of wave harmonics to calculate
 tank.H = 2  # The tank height
 tank.B = 10  # The tank breadth
-tank.U = 15  # The flow speed
+tank.U = 1  # The flow speed
 
-hull = hull.create_hull("data\\models\\5s.stl")
+hull = hull.create_hull("data\\models\\cube.stl.txt")
 sources = sources.create_sources(hull, tank)
 test_source = sources
 test_source.num_sources = 1
@@ -29,29 +29,29 @@ test_source.coords = np.array([[2, 2, -1]])
 waves = wave_system.surface_elevation(tank, sources)
 print("The wave resistance = " + str(sum(waves.Rwm)) + " N")
 
-nx, ny = (300,300)
-x = np.linspace(-2,10,nx)
-y = np.linspace(-5,5,ny)
+nx, ny = (200, 100)
+num_points = nx*ny
+x = np.linspace(-2, 10, nx)
+y = np.linspace(-5, 5, ny)
 xim = waves.xim
 etam = waves.etam
 km = waves.km
 thetam = waves.thetam
-xx, yy = np.meshgrid(x,y)
-z_x_y = np.zeros((nx, ny))
+xx, yy = np.meshgrid(x, y)
+xx = np.reshape(xx.flatten(), (num_points,1))
+yy = yy.flatten()
+yy = np.reshape(yy.flatten(), (num_points,1))
 m = waves.m
 
-@np.vectorize
-def return_elevation(xx, yy):
-    summation = 0
-    if xx>0:
-        for i in m:
-            term1 = xim[i] * np.cos(xx * km[i] * np.cos(thetam[i]))
-            term2 = etam[i] * np.sin(xx * km[i] * np.cos(thetam[i]))
-            term3 = np.cos((i*np.pi*yy)/(tank.B))
-            summation += (term1+term2)*term3
-    return summation
-
-z_x_y = return_elevation(xx,yy)
+term1 = xim*np.cos(xx*km*np.cos(thetam))
+term2 = etam*np.sin(xx*km*np.cos(thetam))
+term3 = np.cos((m*np.pi*yy)/tank.B)
+z_x_y = (term1*term2)*term3
+z_x_y = np.sum(z_x_y, axis=1)
+z_x_y[np.argwhere(xx < 0)] = 0
+z_x_y = z_x_y.reshape(ny, -1)
+xx = xx.reshape(ny, -1)
+yy = yy.reshape(ny, -1)
 
 fig = plt.figure()
 h = plt.contourf(xx, yy, z_x_y)
